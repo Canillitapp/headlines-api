b2cd0ab90ae93ebdc62173113124bdc0ff28cc69
Ezequiel Becerra <ezequiel.becerra@gmail.com>
2017-05-04 21:37:06 -0300

    Syntax sugar

--- database.rb
+++ database.rb
@@ -4,20 +4,20 @@ require 'sinatra/config_file'
 
 config_file 'config/config.yml'
 
-if settings.db_adapter == 'sqlite3' then
+if settings.db_adapter == 'sqlite3'
   ActiveRecord::Base.establish_connection(
-  adapter: settings.db_adapter,
-  database: settings.db_database,
-  pool: 30
-)
+    adapter: settings.db_adapter,
+    database: settings.db_database,
+    pool: 30
+  )
 else
   ActiveRecord::Base.establish_connection(
-  adapter: settings.db_adapter,
-  host: settings.db_host,
-  username: settings.db_username,
-  password: settings.db_password,
-  database: settings.db_database
-)
+    adapter: settings.db_adapter,
+    host: settings.db_host,
+    username: settings.db_username,
+    password: settings.db_password,
+    database: settings.db_database
+  )
 end
 
 # Uncomment this if you want to log some database issue
