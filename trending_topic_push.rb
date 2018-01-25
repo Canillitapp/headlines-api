dd1a35a56148e01b735570cf4584963fffe3e8b2
Ezequiel Becerra <ezequiel.becerra@gmail.com>
2018-01-25 18:44:55 -0300

    script to be used with all users

--- trending_topic_push.rb
+++ trending_topic_push.rb
@@ -1,46 +1,70 @@
 require 'apnotic'
 require './fetcher'
+require './user'
 
-# for production pushes use:
-# https://api.push.apple.com:443
-
-def push_trending_news(date)
-  connection = Apnotic::Connection.new(
-    cert_path: "apns_development.pem",
-    url: "https://api.development.push.apple.com:443"
-  )
-
+def news_to_broadcast(date)
   news = NewsFetcher.new
   trending = news.trending_news(date, 1)
   keyword = trending['keywords'].first
 
   return if keyword.nil?
 
-  item = trending['news'][keyword].first
+  trending['news'][keyword].first
+end
 
-  # create a notification for a specific device token
-  # "aa50ff35cf419e467d6f12408587531e8878ba246ed47909d630d9f90af2d0f3"
-  # "032101db30b0e4e2258e17cb9b0d551e52cd253386c20bb208166507ecda6b61"
-  token = "032101db30b0e4e2258e17cb9b0d551e52cd253386c20bb208166507ecda6b61"
-
+def notification_from_news_item(item, token)
   notification = Apnotic::Notification.new(token)
   notification.alert = {
-    "title" => item['source_name'],
-    "body" => item['title']
+    'title' => item['source_name'],
+    'body' => item['title']
   }
   notification.mutable_content = true
-  notification.category = "news_apn"
+  notification.category = 'news_apn'
   notification.custom_payload = {
-    "media-url" => item['img_url'],
-    "post-id" => item['news_id']
+    'media-url' => item['img_url'],
+    'post-id' => item['news_id']
   }
+  notification
+end
 
-  # send (this is a blocking call)
-  response = connection.push(notification)
+def apns_connection
+  # for production pushes use:
+  # https://api.push.apple.com:443
 
-  # close the connection
+  Apnotic::Connection.new(
+    cert_path: 'apns_development.pem',
+    url: 'https://api.development.push.apple.com:443'
+  )
+end
+
+def single_sync_push_to_token(connection, notification)
+  connection.push(notification)
+  # do something with "response"... I guess?
+end
+
+def single_async_push_to_token(connection, notification)
+  push = connection.prepare_push(notification)
+  push.on(:response) do |response|
+    # do something... I guess?
+  end
+  connection.push_async(push)
+end
+
+def push_trending_news(date)
+  connection = apns_connection
+
+  item = news_to_broadcast(date)
+
+  User.where('ios_device_token IS NOT NULL').each do |u|
+    notification = notification_from_news_item(item, u.ios_device_token)
+    single_async_push_to_token(connection, notification)
+  end
+
+  # wait for all requests to be completed
+  connection.join
+
   connection.close
 end
 
 date = Time.now.strftime('%Y-%m-%d')
-push_trending_news('2018-01-01')
+push_trending_news(date)
