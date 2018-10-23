6f9d7cd850423c611799aa13c6bc308a67b001e5
Ezequiel Becerra <ezequiel.becerra@gmail.com>
2018-10-22 21:36:23 -0300

    chore: adding report debug log

--- interests_report_job.rb
+++ interests_report_job.rb
@@ -1,9 +1,12 @@
 require 'rufus-scheduler'
+require 'logger'
 require_relative 'interests_report_maker'
 
 scheduler = Rufus::Scheduler.new
 
 scheduler.cron '5 * * * *' do
+  logger = Logger.new(STDOUT)
+  logger.debug('starting interests report')
   report_maker = InterestsReportMaker.new
   report_maker.update_reports
 end
