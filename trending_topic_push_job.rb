9e0d484a589211ec5ad2386f7c9bac968dee9a95
Ezequiel Becerra <ezequiel.becerra@gmail.com>
2018-10-23 18:22:02 -0300

    chore: adding logger on push notifications

--- trending_topic_push_job.rb
+++ trending_topic_push_job.rb
@@ -1,9 +1,12 @@
+require 'logger'
 require 'rufus-scheduler'
 require_relative 'trending_topic_push'
 
 scheduler = Rufus::Scheduler.new
+logger = Logger.new(STDOUT)
 
 scheduler.cron '0 10,18 * * *' do
+  logger.debug('starts push notification process')
   date = Time.now.strftime('%Y-%m-%d')
   push_trending_news(date)
 end
