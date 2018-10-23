ad77abbbf0d01ebf0d49cea33c7f027dc50e3431
Ezequiel Becerra <ezequiel.becerra@gmail.com>
2018-10-22 21:42:47 -0300

    chore: silenced debug log

--- fetcher.rb
+++ fetcher.rb
@@ -64,7 +64,7 @@ class NewsFetcher
         link_to_search = link_url.gsub(/^(http|https):\/\//, '')
 
         if News.where('url LIKE ?', "%#{link_to_search}").exists?
-          @logger.debug("#{link_url[0...40]} is duplicated. s:#{source['source_id']}")
+          #@logger.debug("#{link_url[0...40]} is duplicated. s:#{source['source_id']}")
         else
           img_url = NewsFetcher.news_image_url(link_url)
           title = Sanitize.fragment(item.title).strip
