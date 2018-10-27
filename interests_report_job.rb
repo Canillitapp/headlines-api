1f14d37765270649bc132230e602e44417938aea
Ezequiel Becerra <ezequiel.becerra@gmail.com>
2018-10-27 14:36:01 -0300

    chore: changed cron for reports

--- interests_report_job.rb
+++ interests_report_job.rb
@@ -4,8 +4,8 @@ require_relative 'interests_report_maker'
 
 scheduler = Rufus::Scheduler.new
 
-# schedule this at 4:30am everyday
-scheduler.cron '30 4 * * *' do
+# schedule this at 4:30am and 6:30am everyday
+scheduler.cron '30 4,6 * * *' do
   logger = Logger.new(STDOUT)
   logger.debug('starting interests report')
   report_maker = InterestsReportMaker.new
