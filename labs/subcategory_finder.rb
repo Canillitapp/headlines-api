6e695bdbc9909d7bf4e1feb43ba89853513a5476
Ezequiel Becerra <ezequiel.becerra@gmail.com>
2019-04-27 20:30:03 -0300

    better subcategory_finder

--- labs/subcategory_finder.rb
+++ labs/subcategory_finder.rb
@@ -1,29 +1,10 @@
 require File.expand_path(File.dirname(__FILE__) + '/../news.rb')
 require File.expand_path(File.dirname(__FILE__) + '/../subject.rb')
 
-days = [
-  '2019-04-21',
-  '2019-04-20',
-  '2019-04-19',
-  '2019-04-18',
-  '2019-04-17',
-  '2019-04-16',
-  '2019-04-15',
-  '2019-04-14',
-  '2019-04-13',
-  '2019-04-12',
-  '2019-04-11',
-  '2019-04-10',
-  '2019-04-09',
-]
+def subjects_from_day(date)
+  tmp = []
 
-subjects_to_upload = []
-
-days.each do |day|
-  puts day
-  subjects = []
-
-  result = News.trending(day, 12)
+  result = News.trending(date, 12)
 
   result['news'].each do |k, news|
     s = Subject.new(k)
@@ -31,13 +12,28 @@ days.each do |day|
     news.each do |i|
       s.analyse_title(i['title'])
     end
-    subjects << s
+    tmp << s
   end
+
+  tmp
+end
+
+start_date = Date.strptime('2018-01-01', '%Y-%m-%d')
+end_date = Date.today
+date = start_date
+
+subjects_to_upload = []
+
+while date <= end_date
+  puts date
+  subjects = subjects_from_day(date)
 
   subjects.each do |s|
     possible_subjects = s.possible_subjects.select { |i| i[1] > 0.3 && i[0].split(' ').count > 1 }
     possible_subjects.each { |i| subjects_to_upload << i[0] }
   end
+
+  date += 1
 end
 
 subjects_to_upload.uniq.sort.each { |i| puts i }
