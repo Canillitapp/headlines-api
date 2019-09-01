d6d4f96d78a45d542837bc49712376eac1a93631
Ezequiel Becerra <ezequiel.becerra@gmail.com>
2019-09-01 20:07:21 -0300

    feat: smart search

--- news.rb
+++ news.rb
@@ -27,15 +27,15 @@ class News < ActiveRecord::Base
 
   def self.search_news_by_title(search, page)
     offset = (page - 1) * NEWS_LIMIT
-
+    escaped_search = search.gsub(/[\"|“|”]/, '')
     # if the search is surrounded by quotes and it's more than
     # one word (like "Sol Perez"), the app will use a different
     # search query. https://rubular.com/r/JOrjfCmvN97uw6
     news = if search.split(' ').length > 1 && !search.match?(/^[\"|“|”].+[\"|“|”]$/)
-             News.where('MATCH (title) AGAINST (? IN BOOLEAN MODE)', search)
+             News.where('MATCH (title) AGAINST (? IN BOOLEAN MODE)', escaped_search)
            else
              News
-               .where('title LIKE ?', "%#{search}%")
+               .where('title LIKE ?', "%#{escaped_search}%")
                .order('news_id DESC')
            end
 
